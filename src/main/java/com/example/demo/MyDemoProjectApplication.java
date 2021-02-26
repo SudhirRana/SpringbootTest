9515f8aee95924e506aad6c42878f638c3c70009
SudhirRana <=>
2021-02-27 00:50:12 +0530

    First commit

---
+++ src/main/java/com/example/demo/MyDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDemoProjectApplication.class, args);
+		System.out.println("Hello Dear");
+	}
+
+}
