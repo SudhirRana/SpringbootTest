05bfc04c10a5a8833b2e808fc6371163f4d26c0e
SudhirRana <=>
2021-02-27 17:53:13 +0530

    Second commit

--- src/main/java/com/example/demo/MyDemoProjectApplication.java
+++ src/main/java/com/example/demo/MyDemoProjectApplication.java
@@ -9,6 +9,10 @@ public class MyDemoProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyDemoProjectApplication.class, args);
 		System.out.println("Hello Dear");
+		MyBeanclass a = new MyBeanclass();
+		int d = a.MyBeanFunction();
+		System.out.print(d);
+		
 	}
 
 }
